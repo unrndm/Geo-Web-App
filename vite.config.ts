12059714287bcba6d7b66b075b5faa28bf7b0eea
unrndm <unrndm@gmail.com>
2023-10-31 22:39:19 +0300

    fix: added top level await plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import wasm from "vite-plugin-wasm";
 import { optimizeCss } from "carbon-preprocess-svelte";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [wasm(), sveltekit(), process.env.NODE_ENV === "production" && optimizeCss()],
+  plugins: [
+    wasm(),
+    topLevelAwait(),
+    sveltekit(),
+    process.env.NODE_ENV === "production" && optimizeCss()
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"]
   }
