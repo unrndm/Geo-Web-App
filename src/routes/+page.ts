95b96fea31107e7b07d0714e30e39a5718d1fb28
unrndm <unrndm@gmail.com>
2023-11-02 00:57:47 +0300

    fix: fixed building issue

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,1 +1,3 @@
-export const prerender = true;
+// export const prerender = true;
+// breaks with
+// Error [ERR_MODULE_NOT_FOUND]: Cannot find package '/home/unrndm/Projects/geo-web-app/node_modules/geo-web-lib/' imported from /home/unrndm/Projects/geo-web-app/.svelte-kit/output/server/entries/pages/_page.svelte.js
