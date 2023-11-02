6303a79615cb6180b0d21affa7cfac9f0fc02317
unrndm <unrndm@gmail.com>
2023-11-02 05:07:31 +0300

    feat: ironed out some bugs

--- src/lib/Calculator.svelte
+++ src/lib/Calculator.svelte
@@ -1,40 +1,65 @@
 <script lang="ts">
   import { Calculator } from "geo-web-lib";
-  import { Row, Column, TextArea, Button, SkipToContent } from "carbon-components-svelte";
+  import {
+    Row,
+    Column,
+    TextArea,
+    Button,
+    SkipToContent,
+    ExpandableTile,
+    UnorderedList,
+    ListItem
+  } from "carbon-components-svelte";
 
-  // let canvasElement: HTMLCanvasElement;
+  let canvasElement: HTMLCanvasElement;
 
   let calculator = new Calculator();
-  let wkt: string;
-  let HandleInput = (e: Event) => {
-    wkt = (e.target as HTMLTextAreaElement)?.value || "";
-  };
+  // @hmr:keep
+  let wkt: string = "LINESTRING (-1 -1, 4 4)";
 
   let rasterization_result: string | undefined = undefined;
   let HandleCalculate = async (e: Event) => {
-    let data = calculator.rasterize(wkt);
-    rasterization_result = JSON.stringify(data);
-    // let w = data.at(-2)!;
-    // let new_data = data.slice(0, -2);
-    // rasterization_result = JSON.stringify({
-    //   width: w,
-    //   pixels: new_data
-    // });
-    // let h = Math.round(new_data.length / w);
-    // for (let index = 0; index < w; index++) {
-    // const element = array[index];
-    // }
-    // console.log(h, w, new_data);
+    let ctx = canvasElement.getContext("2d")!;
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    let raw_data = calculator.rasterize(wkt);
+    let [w, h, ...clipped_data] = raw_data;
+
+    let data = new Uint8ClampedArray(clipped_data);
+
+    let id = new ImageData(data, w, h);
+    createImageBitmap(id).then((sprite) => {
+      ctx?.drawImage(sprite, 0, 0);
+    });
   };
 </script>
 
+<Row>
+  <Column>
+    <ExpandableTile>
+      <div slot="above">
+        <div>Current limitation</div>
+      </div>
+      <div slot="below">
+        <UnorderedList>
+          <ListItem
+            >X coordinate is reversed and negative coordinates work strangely (caused by need to
+            transfer between wasm & js)</ListItem
+          >
+          <ListItem>Only works with LINESTRING</ListItem>
+          <ListItem>Strange when it comes vertical strings</ListItem>
+        </UnorderedList>
+      </div>
+    </ExpandableTile>
+  </Column>
+</Row>
 <Row>
   <Column>
     <TextArea
       hideLabel
       labelText="A variable WKT definition"
       placeholder="Enter WKT definition..."
-      on:click={HandleInput}
+      bind:value={wkt}
     />
   </Column>
 </Row>
@@ -45,8 +70,8 @@
   </Column>
 </Row>
 <Row>
-  <Column>
-    <!-- <canvas bind:this={canvasElement} /> -->
-    <TextArea disabled labelText="Rasterization result:" value={rasterization_result} />
-  </Column>
+  <!-- <Column> -->
+  <!-- <TextArea disabled labelText="Rasterization result:" value={rasterization_result} /> -->
+  <canvas bind:this={canvasElement} style="flex-grow: 1;" />
+  <!-- </Column> -->
 </Row>
